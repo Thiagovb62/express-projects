6a09db06b5f386be213ff372a0fe3e3e43d394a8
Thiago Viana Bezerra <89490346+Thiagovb62@users.noreply.github.com>
2021-09-29 19:32:32 -0300

    Delete Angular codes directory

--- Angular codes/ex05.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const server = express()
-const router =  require('./ex05_router')
-
-server.use('/api',router)
-server.listen(3000,() => console.log('executando...'))
